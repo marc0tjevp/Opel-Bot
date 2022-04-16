60d2bebf205aa3e9169b8c7f0852364a86bca198
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 22:33:03 +0200

    Just log the damn body

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -3,8 +3,8 @@ const client = require("../bot");
 
 routes.post("/", (req, res) => {
   console.dir(req.body);
-  client.channels.cache.get(`962049203650064385`).send({ payload: req.body });
-  return res.status(200).json(req.body).end();
+  // client.channels.cache.get(`962049203650064385`).send({ payload: req.body });
+  return res.status(200).end();
 });
 
 module.exports = routes;
