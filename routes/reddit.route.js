5f3077bcc5664ace52519f244453838e3f895745
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 21:53:32 +0200

    debug

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -2,6 +2,7 @@ const routes = require("express").Router();
 const client = require("../index");
 
 routes.post("/", (req, res) => {
+  console.dir(client);
   client.channels.cache.get(`962049203650064385`).send(message);
   return res.status(200).json(req.body).end();
 });
