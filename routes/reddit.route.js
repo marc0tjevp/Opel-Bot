226bba41163aefc7c196fa286683e069c170466b
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 22:19:19 +0200

    string

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -2,8 +2,8 @@ const routes = require("express").Router();
 const client = require("../bot");
 
 routes.post("/", (req, res) => {
-  console.dir(client);
-  client.channels.cache.get(`962049203650064385`).send(req.body);
+  const body = req.body.toString();
+  client.channels.cache.get(`962049203650064385`).send(body);
   return res.status(200).json(req.body).end();
 });
 
