c2690669c17da1723c4becd2d715fce760e3bdf5
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 22:21:00 +0200

    stringify

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -2,7 +2,7 @@ const routes = require("express").Router();
 const client = require("../bot");
 
 routes.post("/", (req, res) => {
-  const body = req.body.toString();
+  const body = JSON.stringify(req.body);
   client.channels.cache.get(`962049203650064385`).send(body);
   return res.status(200).json(req.body).end();
 });
