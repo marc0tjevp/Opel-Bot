cbed84615fb91985eaa640d8712cb309fb4f8866
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 22:55:25 +0200

    test embed

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -1,9 +1,27 @@
+const { MessageEmbed } = require("discord.js");
 const routes = require("express").Router();
 const client = require("../bot");
 
 routes.post("/", (req, res) => {
+  const { title, imageUrl, content, postUrl, author } = req.body;
+
+  const exampleEmbed = new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle(title)
+    .setURL(postUrl)
+    .setAuthor({
+      name: author,
+    })
+    .setDescription(content)
+    .setThumbnail(imageUrl)
+    .setTimestamp();
+
   console.dir(req.body);
-  client.channels.cache.get(`962049203650064385`).send(req.body);
+
+  client.channels.cache
+    .get(`962049203650064385`)
+    .send({ embeds: [exampleEmbed] });
+
   return res.status(200).end();
 });
 
