c8f060636e0b0b12b14aaf242104ee9d0b413712
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 22:16:28 +0200

    Fix message

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -3,7 +3,7 @@ const client = require("../bot");
 
 routes.post("/", (req, res) => {
   console.dir(client);
-  client.channels.cache.get(`962049203650064385`).send(message);
+  client.channels.cache.get(`962049203650064385`).send(req.body);
   return res.status(200).json(req.body).end();
 });
 
