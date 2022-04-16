82346b69ddb38585a9406984ff5d810085b60dbe
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 21:17:36 +0200

    Use route instead

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ routes.get("/", (req, res) =>
   res.status(200).json({ message: "Hello World!" })
 );
 
-routes.post("/reddit", redditRoutes);
+routes.use("/reddit", redditRoutes);
 
 routes.use("*", (req, res) =>
   res.status(404).json({ message: "Not found" }).end()
