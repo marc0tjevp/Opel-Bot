0ae45c1f41d594811561d6ac5505735280fc6ef5
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 21:51:57 +0200

    move bot

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-const { Client, Intents, MessageEmbed } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-require("./register.js");
-require("./server.js");
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  // PING
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
-  }
-
-  // CMDS
-  if (interaction.commandName === "cmds") {
-    const embed = new MessageEmbed()
-      .setColor("#000000")
-      .setTitle("Bot Commands")
-      .addFields({ name: "Is you alive?", value: "`ping`" })
-      .addFields({ name: "Your'e looking at it", value: "`cmds`" });
-
-    interaction.reply({ embeds: [embed], ephemeral: true });
-  }
-});
-
-client.login(process.env.TOKEN);
-
-module.exports = client;
+require("./register");
+require("./bot");
+require("./server");
