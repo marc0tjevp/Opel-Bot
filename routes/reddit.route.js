e2aa7e803b7c527a7496456a02cf7863f45ddbd4
Marco van Poortvliet <marc0tjevp@gmail.com>
2022-04-16 22:31:54 +0200

    wrap object

--- routes/reddit.route.js
+++ routes/reddit.route.js
@@ -3,7 +3,7 @@ const client = require("../bot");
 
 routes.post("/", (req, res) => {
   console.dir(req.body);
-  client.channels.cache.get(`962049203650064385`).send(req.body);
+  client.channels.cache.get(`962049203650064385`).send({ payload: req.body });
   return res.status(200).json(req.body).end();
 });
 
